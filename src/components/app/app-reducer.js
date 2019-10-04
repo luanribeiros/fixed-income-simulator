903d75dc023e691ce8a711c1894ab82705ea074d
Luan Ribeiro <luanribeiros@gmail.com>
2019-10-04 01:06:02 -0300

    ajustes e novas incrementação de métodos para o cálculo

--- src/components/app/app-reducer.js
+++ src/components/app/app-reducer.js
@@ -1,48 +1,10 @@
-const INITIAL_STATE = {
-  datas: {
-      minInvestValue: 1,
-      maxInvestValue: 1000000,
-      defaultInvestValue: 100000,
-      stepInvestValue: 5000,
+const INITIAL_STATE = {list: []}
 
-      minPeriod: 1,
-      maxPeriod: 120,
-      defaultPeriod: 25,
-      stepPeriod: 1,
+const FatorCotizacaoCDI = () => {}
 
-      minCdiPercentRate: 0.1,
-      maxCdiPercentRate: 2,
-      defaultCdiPercentRate: 1,
-      stepCdiPercentRate: 0.01,
+const FatorCotizacaoIPCA = () => {}
 
-      minCdiPlusRate: -0.03,
-      maxCdiPlusRate: 0.03,
-      defaultCdiPlusRate: 0.01,
-      stepCdiPlusRate: 0.001,
-
-      minIpcaPlusRate: 0.01,
-      maxIpcaPlusRate: 0.15,
-      defaultIpcaPlusRate: 0.05,
-      stepIpcaPlusRate: 0.005,
-
-      minFixedRate: 0.03,
-      maxFixedRate: 0.15,
-      defaultFixedRate: 0.08,
-      stepFixedRate: 0.005,
-
-      minDiRate: 0.01,
-      maxDiRate: 0.2,
-      defaultDiRate: 0.059,
-      stepDiRate: 0.001,
-
-      minIpcaRate: 0.01,
-      maxIpcaRate: 0.2,
-      defaultIpcaRate: 0.035,
-      stepIpcaRate: 0.001,
-
-      rateOptions: ["% do CDI", "CDI + %", "IPCA + %", "Prefixado"]
-    }
-}
+const FatorCotizacaoPrefixado = () => {}
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
